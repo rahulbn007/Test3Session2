a55f1b1abf0cabafb54d2992e30733254e023d84
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-03-05 17:39:48 +0000

    .

--- p1final.c
+++ p1final.c
@@ -1,1 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter  the first fraction:\n");
+  scanf("%d%d",num1,den1);
+  printf("Enter  the second fraction:\n");
+  scanf("%d%d",num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  if(den1==den2)
+  {
+    *num3=num1+num2;
+    *den3=den1;
+  }
+    *num3=(num1*den2+num2*den1);
+    *den3=den1*den2;
+  for(int i=2;i<8;i++)
+    {
+    while(*num3%i==0 && *den3%i==0)
+      {
+        *num3=(*num3)/i;
+        *den3=(*den3)/i;
+      }
+    }
+  if(*num3==*den3)
+    *num3=*den3=1;
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf(" %d/%d + %d/%d = %d/%d\n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,num2,num3,den1,den2,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+typedef struct _fraction
+{
+  int num,den;
+}Fraction;
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Enter  the first fraction:\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+  float x,y,z;
+  x=(float)f1.num/f1.den;
+  y=(float)f2.num/f2.den;
+  z=(float)f3.num/f3.den;
+ if(x<y && x<z)
+   return f1;
+ else if(y<x && y<z)
+   return f2;
+  else
+   return f3;
+}
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+{
+  printf("The largest of three fractions : %d/%d %d/%d %d/%d is \n %d/%d\n",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,largest.num,largest.den);
+}
+int main()
+{
+  Fraction f1,f2,f3,largest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  largest=Largest_fraction(f1,f2,f3);
+  output(f1,f2,f3,largest);
+  return 0;
+}
